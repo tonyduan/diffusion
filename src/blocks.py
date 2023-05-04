1e91704544f6c94b29a39db96a220f06d20f13ab
Tony Duan <tony@tonyduan.com>
2023-05-04 01:10:30 -0700

    Add ViT.

--- src/blocks.py
+++ src/blocks.py
@@ -1,6 +1,6 @@
 import math
 
-from einops import rearrange
+from einops import parse_shape, rearrange
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -19,26 +19,29 @@ def unsqueeze_as(tensor, target_tensor):
     return tensor
 
 
-class PositionalEncoding(nn.Module):
+class PositionalEmbedding(nn.Module):
     def __init__(self, dim, max_length=10000):
         super().__init__()
-        encoding = torch.zeros(max_length, dim)
-        position = torch.arange(0, max_length).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, dim, 2) * (-math.log(max_length / 2 / math.pi) / dim))
-        encoding[:, 0::2] = torch.sin(position * div_term)
-        encoding[:, 1::2] = torch.cos(position * div_term)
-        self.register_buffer("encoding", encoding)
+        self.register_buffer("embedding", self.make_embedding(dim, max_length))
 
     def forward(self, x):
-        return self.encoding[x]
+        return self.embedding[x]
+
+    @staticmethod
+    def make_embedding(dim, max_length=10000):
+        embedding = torch.zeros(max_length, dim)
+        position = torch.arange(0, max_length).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, dim, 2) * (-math.log(max_length / 2 / math.pi) / dim))
+        embedding[:, 0::2] = torch.sin(position * div_term)
+        embedding[:, 1::2] = torch.cos(position * div_term)
+        return embedding
 
 
 class FFN(nn.Module):
-
     def __init__(self, in_dim, embed_dim):
         super().__init__()
         self.init_embed = nn.Linear(in_dim, embed_dim)
-        self.time_embed = PositionalEncoding(embed_dim)
+        self.time_embed = PositionalEmbedding(embed_dim)
         self.model = nn.Sequential(
             nn.Linear(embed_dim, embed_dim),
             nn.ReLU(),
@@ -133,7 +136,7 @@ class UNet(nn.Module):
         super().__init__()
 
         self.init_embed = nn.Conv2d(in_dim, embed_dim, 1)
-        self.time_embed = PositionalEncoding(embed_dim)
+        self.time_embed = PositionalEmbedding(embed_dim)
 
         self.down_blocks = nn.ModuleList()
         self.up_blocks = nn.ModuleList()
@@ -198,4 +201,153 @@ class UNet(nn.Module):
                 x = block(x, t)
             else:
                 x = block(x)
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    Multi-Head Attention [Vaswani et al. NeurIPS 2017].
+    Scaled dot-product attention is performed over V, using K as keys and Q as queries.
+        MultiHeadAttention(Q, V) = FC(SoftMax(1/√d QKᵀ) V) (concatenated over multiple heads),
+    Notes
+    -----
+    (1) Q, K, V can be of different dimensions. Q and K are projected to dim_a and V to dim_o.
+    (2) We assume the last and second last dimensions correspond to the feature (i.e. embedding)
+        and token (i.e. words) dimensions respectively.
+    """
+    def __init__(self, dim_q, dim_k, dim_v, num_heads=8, dropout_prob=0.1, dim_a=None, dim_o=None):
+        super().__init__()
+        if dim_a is None:
+            dim_a = dim_q
+        if dim_o is None:
+            dim_o = dim_q
+        self.dim_a, self.dim_o, self.num_heads = dim_a, dim_o, num_heads
+        self.fc_q = nn.Linear(dim_q, dim_a, bias=True)
+        self.fc_k = nn.Linear(dim_k, dim_a, bias=True)
+        self.fc_v = nn.Linear(dim_v, dim_o, bias=True)
+        self.fc_o = nn.Linear(dim_o, dim_o, bias=True)
+        self.dropout = nn.Dropout(dropout_prob)
+        for module in (self.fc_q, self.fc_k, self.fc_v, self.fc_o):
+            nn.init.xavier_normal_(module.weight)
+            nn.init.constant_(module.bias, 0.)
+
+    def forward(self, q, k, v, mask=None):
+        """
+        Perform multi-head attention with given queries and values.
+        Parameters
+        ----------
+        q: (bsz, tsz, dim_q)
+        k: (bsz, tsz, dim_k)
+        v: (bsz, tsz, dim_v)
+        mask: (bsz, tsz) or (bsz, tsz, tsz), where 1 denotes keep and 0 denotes remove
+        Returns
+        -------
+        O: (bsz, tsz, dim_o)
+        """
+        bsz, tsz, _ = q.shape
+        q, k, v = self.fc_q(q), self.fc_k(k), self.fc_v(v)
+        q = torch.cat(q.split(self.dim_a // self.num_heads, dim=-1), dim=0)
+        k = torch.cat(k.split(self.dim_a // self.num_heads, dim=-1), dim=0)
+        v = torch.cat(v.split(self.dim_o // self.num_heads, dim=-1), dim=0)
+        a = q @ k.transpose(-1, -2) / self.dim_a ** 0.5
+        if mask is not None:
+            assert mask.ndim in (2, 3)
+            if mask.ndim == 3:
+                mask = mask.repeat(self.num_heads, 1, 1)
+            if mask.ndim == 2:
+                mask = mask.unsqueeze(-2).repeat(self.num_heads, tsz, 1)
+            a.masked_fill_(mask == 0, -65504)
+        a = self.dropout(torch.softmax(a, dim=-1))
+        o = self.fc_o(torch.cat((a @ v).split(bsz, dim=0), dim=-1))
+        return o
+
+
+class PositionwiseFFN(nn.Module):
+    """
+    Position-wise FFN [Vaswani et al. NeurIPS 2017].
+    """
+    def __init__(self, dim, hidden_dim, dropout_prob=0.1):
+        super().__init__()
+        self.fc1 = nn.Linear(dim, hidden_dim, bias=True)
+        self.fc2 = nn.Linear(hidden_dim, dim, bias=True)
+        self.dropout = nn.Dropout(dropout_prob)
+        for module in (self.fc1, self.fc2):
+            nn.init.kaiming_normal_(module.weight)
+            nn.init.constant_(module.bias, 0.)
+
+    def forward(self, x):
+        return self.fc2(self.dropout(F.relu(self.fc1(x))))
+
+
+class EncoderBlock(nn.Module):
+    """
+    Transformer encoder block [Vaswani et al. NeurIPS 2017].
+    Note that this is the pre-LN version [Nguyen and Salazar 2019].
+    """
+    def __init__(self, dim, hidden_dim, num_heads=8, dropout_prob=0.1):
+        super().__init__()
+        self.attn = MultiHeadAttention(dim, dim, dim, num_heads, dropout_prob)
+        self.ffn = PositionwiseFFN(dim, hidden_dim, dropout_prob)
+        self.dropout = nn.Dropout(dropout_prob)
+        self.ln1 = nn.LayerNorm(dim)
+        self.ln2 = nn.LayerNorm(dim)
+        self.mlp_time_1 = nn.Sequential(
+            nn.Linear(dim, dim),
+            nn.ReLU(),
+            nn.Linear(dim, dim),
+        )
+        self.mlp_time_2 = nn.Sequential(
+            nn.Linear(dim, dim),
+            nn.ReLU(),
+            nn.Linear(dim, dim),
+        )
+
+    def forward(self, x, t, mask=None):
+        x_ = self.ln1(x)
+        x_ = x_ + self.mlp_time_1(t).unsqueeze(1)
+        x = x + self.dropout(self.attn(x_, x_, x_, mask))
+        x_ = self.ln2(x)
+        x_ = x_ + self.mlp_time_2(t).unsqueeze(1)
+        x = x + self.dropout(self.ffn(x_))
+        return x
+
+
+class ViT(nn.Module):
+    """
+    Simple version of Vision Transformer [Dosovitsky et al. 2020].
+    """
+    def __init__(self, in_dim, embed_dim, num_layers, patch_shape):
+        super().__init__()
+
+        self.patch_h, self.patch_w = patch_shape
+        self.patch_embed = nn.Linear(in_dim * math.prod(patch_shape), embed_dim, bias=True)
+        self.time_embed = nn.Parameter(PositionalEmbedding.make_embedding(embed_dim))
+        self.pos_embed = nn.Parameter(PositionalEmbedding.make_embedding(embed_dim))
+
+        self.blocks = nn.ModuleList([
+            EncoderBlock(embed_dim, 4 * embed_dim) for _ in range(num_layers)
+        ])
+        self.ln = nn.LayerNorm(embed_dim)
+        self.out_embed = nn.Linear(embed_dim, in_dim * math.prod(patch_shape), bias=True)
+
+    def forward(self, x, t):
+
+        shape_info = parse_shape(x, "b c h w")
+
+        x = rearrange(x, "b c (h ph) (w pw) -> b (h w) (ph pw c)", ph=self.patch_h, pw=self.patch_w)
+        x = self.patch_embed(x) + rearrange(self.pos_encoding[:x.shape[1]], "t c -> () t c")
+        t = self.time_embed[t]
+
+        for block in self.blocks:
+            x = block(x, t)
+
+        x = self.ln(x)
+        x = self.out_embed(x)
+
+        x = rearrange(x, "b (h w) (ph pw c) -> b c (h ph) (w pw)",
+            h=shape_info["h"] // self.patch_h,
+            w=shape_info["w"] // self.patch_w,
+            ph=self.patch_h,
+            pw=self.patch_w,
+        )
         return x
